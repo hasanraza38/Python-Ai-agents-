bf739090b46ed54714f2aceb4f1f83cbe9ab17b6
HASAN RAZA <92hasanraza689@gmail.com>
2025-04-21 02:07:19 +0500

    class 2

--- 02/index.py
+++ 02/index.py
@@ -1,9 +1,177 @@
-print("helo world class 2")
+# print("hello world class 2")
 
                                             # VARIABLES
+# message = "hello world"
+# print(message)
+# print(message)
+# print(message)
+# print(message)
 
-message = "hello world"
-print(message)
-print(message)
-print(message)
-print(message)
+# memory === RAM not hard disk
+
+                                                  # ----data types-----
+                                            # 1-string
+# name="hasan raza"
+                                            # 2-number/integar
+# age=19
+                                            # 3-boolean
+# isloggedIn=False      
+                                            # 4- float
+# height=5.9
+  
+
+                                    #-------concatinate-------
+# firstName="hasan"
+# lastName="raza"
+# fullName=firstName+lastName
+# # fullName=firstName+" "+lastName
+# print(fullName)
+
+
+
+                                #  ------f-string(formated string)-------
+
+# firstName="hasan"
+# lastName="raza"
+# fullName= f" hello {firstName} {lastName}"
+# print(fullName)
+                                       # --------------------------
+
+# firstName="hasan"
+# lastName="raza"
+# age=19
+# print(f"hello {firstName} {lastName} ypy age is {age}")
+
+                                       # --------------------------
+
+
+# firstName="hasan"
+# lastName="raza"
+# age=19
+
+# print(type(firstName))
+# print(type(age))
+# print(id(firstName)) (location batyega memory mein)
+
+
+                              #---------- operators-------------
+                                     
+                                    #  -airthmetic operator 
+                  # (+ , - , / , *, %(modulus remainder return), //(floor division),** (exponential))
+
+      #addition
+# a= 10
+# b=20
+# c= a + b
+# print(c)
+
+      #substration
+
+# a= 10
+# b=20
+# c= a - b
+# print(c)
+
+              # multiplication
+# a= 10
+# b=20
+# c= a * b
+# print(c)
+
+              # division
+
+# a= 10
+# b=20
+# c= a / b
+# print(c)
+
+              # modulus
+
+# a= 10
+# b=3
+# c= a % b
+# print(c)    
+
+               # floor division
+
+# a= 10
+# b=20
+# c= a // b
+# print(c)    
+
+               # exponential
+
+# a= 2
+# b=2
+# c= a ** b
+# print(c)    
+
+
+                                           #errors
+
+# a= 10
+# b=20
+# c= "a" + b
+# print(c)
+
+       #    -------------------
+
+# a= 10
+# b="20"
+# c= a + b
+# print(c)
+ 
+     #    -------------------
+         
+# a= 10
+# b=3
+# print(10 + b - 2 * a + 10)
+
+      #    -------------------
+
+                                    #-comparison operator (hamesha boolean return karega)
+                   #( ==, !=, >(greater than), < (less then), >=(ya tou barabar ha ya bara ha), <=)
+
+
+                             #"=="
+# a=10
+# b=20
+# c= a==b
+# print(c)
+                             #"!="
+
+# a=10
+# b=20
+# c= a!=b
+# print(c)
+                             #">"
+# a=10
+# b=20
+# c= a>b
+# print(c)
+
+
+                             #"<"
+# a=10
+# b=20
+# c= a<b
+# print(c)
+                             #">="
+# a=10
+# b=10
+# c= a>=b
+# print(c)
+                             #"<="
+# a=9
+# b=20
+# c= a<=b
+# print(c)
+                       
+
+                                  #logical operator
+                                   #(and, or, not)
+
+
+
+
+                                    #  -assigning operator

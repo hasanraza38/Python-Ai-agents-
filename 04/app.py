3f444fed077a4187c6deb9c34cd08ff85c45a307
HASAN RAZA <92hasanraza689@gmail.com>
2025-05-06 02:06:26 +0500

    class 4

---
+++ 04/app.py
@@ -0,0 +1,72 @@
+# print("hello world")
+
+
+# age =  input("Please enter age : ")
+
+# if age.isdigit():
+#     print(age)
+# else:
+#     print("please enter valid input")
+
+
+
+
+
+
+# runs = input("enter total runs :")
+# total_balls = input("enter total runs :")
+
+
+
+# if runs.isdigit() and total_balls.isdigit():
+#         #  true             true               true
+#    if runs >= 50 or total_balls < runs and sixes == True:
+#       sixes = runs >= 30 
+#       print("king")
+#    else:
+#       print("🔔")
+# else:
+   
+#    print("please enter valid input")
+    
+
+
+               #error
+
+# Traceback (most recent call last):
+#   File "E:\Python(Ai-Agents)\04\app.py", line 23, in <module>
+#     if runs >= 50 or total_balls < runs and sixes == True:
+#        ^^^^^^^^^^
+# TypeError: '>=' not supported between instances of 'str' and 'int'
+
+
+
+
+
+                               #data type list 
+
+fruits = [ "apple" , "banana", "gava", "strawberry"]
+
+
+# last me se remove karega agar index number na diya hoo aur agr index num diya ho tou index number k hisaab se remove karega
+# fruits.pop()
+
+
+# last me add karega 
+# fruits.append("mango")
+
+
+# index number k hisab se add karega
+# fruits.insert(2,"mango")
+
+
+# name k hisaab se remove karega
+# fruits.remove("mango")
+
+
+print(fruits)
+
+
+
+
+['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getstate__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'] 

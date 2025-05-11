3fb972e042117f75ab255afada57b25a5006c749
HASAN RAZA <92hasanraza689@gmail.com>
2025-05-11 22:05:55 +0500

    class 05

---
+++ 05/app.py
@@ -0,0 +1,88 @@
+# print("hello world class 5")
+
+
+
+
+                                        #  functions
+
+
+
+# def sum():
+#     print(2+2)
+
+# sum()
+
+
+
+# def sum(num1, num2):
+#     print(num1 + num2)
+
+# sum(5,10)  #positional argument
+# sum(num1=5,num2=10)  #keyword argument
+
+
+
+
+# def calculate(num1,operator,num2):
+#     if operator == "+":
+#         print(num1 + num2)
+#     elif operator == "-":
+#         print(num1 - num2)
+#     elif operator == "/":
+#         print(num1 / num2)
+#     elif operator == "*":
+#         print(num1 * num2)
+#     else:
+#        print("please pass a valid arguments")
+
+
+# num1=int(input("please enter a number one:"))
+# num2=int(input("please enter a number two:"))
+# operartor = input('please enter a Operrator:')
+
+# calculate(num1,operartor,num2)  
+
+
+
+
+# default Parameter (agar parameter ek se ziada hoo tou default parameter hamesha first pr set hoga )
+# def greedUser(name="User"):
+    # print("hello", {name})
+
+# greedUser("Hasan")
+# default Parameter
+
+
+
+
+
+
+
+
+                         # lambda function
+# sum = lambda num1,num2: num1 + num2
+# print(sum(2,2))
+                         # lambda function
+
+
+
+
+
+
+
+
+
+# def calculateCalories(food1,food2, food3):
+
+
+
+# foodArr = ["Pizza","Burger","Biryani", "Haleem", "Broast"]
+# input1 = input(f""" 
+# 2:{foodArr[1]}
+# 3:{foodArr[2]}
+# 4:{foodArr[3]}
+# 5:{foodArr[4]}
+#  Please enter you food :
+# """)
+
+# print(input1)
